c88860b7c95476c31721278a654db57433b6e0a1
이준용 <your_email@example.com>
2025-04-06 02:29:34 +0900

    컨버전컨트롤러 수정

--- Twitters/Controller/ConversationController.swift
+++ Twitters/Controller/ConversationController.swift
@@ -19,14 +19,20 @@ final class ConversationController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigatinBar()
         configureUI()
     }
 
     // MARK: - Selectors
 
     // MARK: - UI Configurations
+
+    private func configureNavigatinBar() {
+        self.navigationItem.title = "Conversation"
+    }
+
     private func configureUI() {
-        self.view.backgroundColor = .systemYellow
+        self.view.backgroundColor = .white
     }
 
     // MARK: - Functions
